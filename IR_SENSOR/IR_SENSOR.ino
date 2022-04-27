202d6fde768a874f0fc964f4e0b382578b6876d4
Prashanna Rangan <43216411+Prashanna08@users.noreply.github.com>
2022-04-27 20:05:41 +0530

    Add files via upload

---
+++ IR_SENSOR/IR_SENSOR.ino
@@ -0,0 +1,18 @@
+void setup() 
+{
+pinMode(4,INPUT);
+pinMode(12,OUTPUT);//LED
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  if(digitalRead(4)==LOW)
+  {
+    digitalWrite(12,HIGH);
+  }
+  else
+  {
+  digitalWrite(12,LOW);
+  }
+}
---
+++ STEPPER_MOTOR/STEPPER_MOTOR.ino
@@ -0,0 +1,26 @@
+#include <Stepper.h>
+
+// Defines the number of steps per rotation
+const int stepsPerRevolution = 360;
+
+// Creates an instance of stepper class
+// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
+Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
+
+void setup() 
+{
+  // Nothing to do (Stepper Library sets pins as outputs)
+}
+
+void loop() 
+{
+  // Rotate CW slowly
+  myStepper.setSpeed(100);
+  myStepper.step(stepsPerRevolution);
+  //delay(1000);
+  
+  // Rotate CCW quickly
+  //myStepper.setSpeed(100);
+  //myStepper.step(-stepsPerRevolution);
+  //delay(1000);
+}
