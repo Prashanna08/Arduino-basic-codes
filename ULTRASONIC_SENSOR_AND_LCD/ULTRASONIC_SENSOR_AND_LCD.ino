4574a07d4618a3cf815276df88c9ffeab28b64d8
Prashanna Rangan <43216411+Prashanna08@users.noreply.github.com>
2022-04-28 00:29:35 +0530

    Add files via upload

---
+++ ULTRASONIC_SENSOR_AND_LCD/ULTRASONIC_SENSOR_AND_LCD.ino
@@ -0,0 +1,25 @@
+#include<UltraDistSensor.h> //Library for ultrasonic sensor
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+UltraDistSensor mysensor;
+float reading;
+
+void setup() 
+{
+    lcd.init(); //Initialize the lcd 
+    lcd.backlight();
+    Serial.begin(9600);
+    mysensor.attach(8,9);//8 - Trigger pin , 9 - Echo pin
+}
+
+void loop() 
+{
+    reading=mysensor.distanceInCm();
+    lcd.setCursor(0,0);
+    lcd.print("Distance :- ");
+    lcd.print(reading);
+    delay(1000);
+}
